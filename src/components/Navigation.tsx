11076c02480608f624806cad64afce02ebc6b5a5
Brandon Pessman <pessman.brandon@gmail.com>
2024-06-18 18:37:47 -0500

    base navigation

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,28 @@
+function Navigation() {
+  return (
+    <>
+      <h1 style={{ margin: 0 }}>Brandon Pessman</h1>
+      <div>
+        <a href="/">blog</a> | <a href="/">projects</a> |{" "}
+        <a href="/">toolbox</a> | <a href="/">reading list</a>
+        <div></div>
+        <a className="a-secondary" href="https://github.com/Braymen">
+          github
+        </a>{" "}
+        |{" "}
+        <a
+          className="a-secondary"
+          href="https://www.linkedin.com/in/brandonpessman/"
+        >
+          linkedin
+        </a>{" "}
+        |{" "}
+        <a className="a-secondary" href="https://x.com/brandonpessman">
+          twitter
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default Navigation;
